a887c4ab16fe976ee072b387d6c03d6c6d6a22b6
Tim Gallant <me@timgallant.us>
2021-07-18 22:46:23 -0700

    removes long command description

--- cmd/db2jsonschema/main.go
+++ cmd/db2jsonschema/main.go
@@ -71,13 +71,7 @@ func HandleGenerate(cmd *cobra.Command, args []string) {
 var rootCmd = &cobra.Command{
 	Use:   "db2jsonschema",
 	Short: "Generate JSON Schema definitions from database tables",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: HandleGenerate,
+	Run:   HandleGenerate,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
